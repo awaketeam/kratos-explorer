d8eee5a7fe38e726be50d72b1e7ce0492c8c122f
ylic <ylic@263.net>
2020-10-16 09:08:42 +0800

    update

--- imports/ui/components/Footer.jsx
+++ imports/ui/components/Footer.jsx
@@ -20,10 +20,10 @@ export default class Footer extends React.Component {
         return (
             <div>
                 <Navbar color="light" light expand="md" fixed="bottom" id="footer" className="d-none d-md-flex">
-                    <span className="text-muted"><a href="https://raw.githubusercontent.com/forbole/big_dipper/master/LICENSE" target="_blank"><T>navbar.license</T></a> &copy;2018-{moment().format('YYYY')}. </span>
+                    <span className="text-muted"><a href="https://raw.githubusercontent.com/awaketeam/kratos-explorer/blob/main/LICENSE" target="_blank"><T>navbar.license</T></a> &copy;2019-{moment().format('YYYY')} By Block-X.io </span>
                     <Nav className="ml-auto" navbar>
                         <NavItem>
-                            <NavLink href="https://www.github.com/forbole/big_dipper" target="_blank"><i className="fab fa-github"></i> <T>navbar.forkMe</T></NavLink>
+                            <NavLink href="https://www.github.com/awaketeam/kratos-explorer" target="_blank"><i className="fab fa-github"></i> <T>navbar.forkMe</T></NavLink>
                         </NavItem>
                     </Nav>
                 </Navbar>
